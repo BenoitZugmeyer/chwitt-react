88f9152ac2cac95281e882a7da19ded695e974e3
Benoît Zugmeyer <bzugmeyer@gmail.com>
2015-03-11 18:27:47 +0100

    Improve glob to support escapes

--- src/glob.jsx
+++ src/glob.jsx
@@ -2,13 +2,33 @@
 
 module.exports = function glob(s) {
     var regex = s
-    .replace(/\\/g, '\\\\')
-    .replace(/([\-.*+\?\^$()\[\]{}|])/g, '\\$1')
-    .replace(/\\\*/g, '.*')
-    .replace(/\\\?/g, '.')
-    .replace(/\\\{/g, '(')
-    .replace(/\\\}/g, ')')
-    .replace(/,/g, '|');
+    .replace(/([\\\-.*+\?\^$()\[\]{}|])/g, '\\$1')
+    .replace(/(\\*)(\\[*?{}\\]|,)/g, function (whole, slashes, ch) {
+        slashes = slashes.slice(slashes.length / 2);
+        return slashes + (
+            slashes.length % 2 === 1 ? ch.slice(-1) :
+            ch === ',' ? '|' :
+            ch === '\\*' ? '.*' :
+            ch === '\\?' ? '.' :
+            ch === '\\{' ? '(' :
+            ch === '\\}' ? ')' :
+                ''
+        );
+    });
 
     return new RegExp('^' + regex + '$');
 };
+
+// var assert = require('assert');
+// var glob = module.exports;
+
+// assert.equal(String(glob('?')), '/^.$/');
+// assert.equal(String(glob('{a,b}')), '/^(a|b)$/');
+// assert.equal(String(glob('\\\\')), '/^\\\\$/');
+// assert.equal(String(glob('\\?')), '/^\\?$/');
+// assert.equal(String(glob('\\*')), '/^\\*$/');
+// assert.equal(String(glob('\\\\*')), '/^\\\\.*$/');
+// assert.equal(String(glob('\\\\\\*')), '/^\\\\\\*$/');
+// assert.equal(String(glob('\\,')), '/^\\,$/');
+// assert.equal(String(glob('\\\\,')), '/^\\\\|$/');
+// console.log('OK');
